1d2433f1e301129068b70247f8a221ec9ff2cd8a
jouhy <ethda11@kookmin.ac.kr>
2024-02-21 11:32:10 +0900

    Feat : Add spectrogram filtering - - #7

---
+++ sf2f/datasets/filter_pickle.py
@@ -0,0 +1,29 @@
+import pickle
+import os
+
+
+def filter_pickle(mel_pickle):
+    """
+    Load a mel pickle file and remove the file if it is empty
+
+    Args:
+        mel_pickle: str, path to the mel pickle file
+    Returns:
+        None
+    """
+    file = open(mel_pickle, 'rb')
+    try:
+        data = pickle.load(file)
+    except:
+        print('Error loading:', mel_pickle)
+        os.remove(mel_pickle)
+        
+    file.close()
+
+
+dir_path = './data/VoxCeleb/vox1/mel_spectrograms'
+for dir in os.listdir(dir_path):
+    data_path = os.path.join(dir_path, dir) # 사람 파일
+    for id in os.listdir(data_path):
+        data = os.path.join(data_path, id) #피클파일
+        filter_pickle(data)
