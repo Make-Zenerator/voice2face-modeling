255f69158f69e3980ecaa3d0e4f6946691f29fd6
IamJiyong <din2346@gmail.com>
2024-02-26 00:53:30 +0900

    Fix : remove raw data after preprocessing - - #10

--- sf2f/scripts/convert_wav_to_mel.py
+++ sf2f/scripts/convert_wav_to_mel.py
@@ -157,6 +157,8 @@ class WavConvertor:
             for i, info in enumerate(infos):
                 self.convert_identity(info[0], info[1], info[2])
                 print('job #{} prcess {} {} done'.format(job_id, i, info[0]))
+                shutil.rmtree(info[0]) # remove complete file
+
                 
     def convert_wav_to_mel(self, n_jobs=1):
 
@@ -206,7 +208,7 @@ def main():
     #    'data/VoxCeleb/raw_wav/vox1/dev/id10001/',
     #    './data/test', 'vox1')
 
-    dir_path = '../data/VoxCeleb/vox1/mel_spectrograms' # wav_convertor.vox1_mel
+    dir_path = './data/VoxCeleb/vox1/mel_spectrograms' # wav_convertor.vox1_mel
     filtering_pickle(dir_path)
 
 
