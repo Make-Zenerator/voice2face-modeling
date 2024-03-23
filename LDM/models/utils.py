bbe47fa04af2f673208996867c61df05b83a94fd
taeyang916 <taeyang916@naver.com>
2024-03-23 15:24:44 +0000

    feat : add model build
    - config 파일 수정 진행 중
    - utils.py build_model 구현 중
    
    - #22

--- LDM/models/utils.py
+++ LDM/models/utils.py
@@ -1,19 +1,25 @@
 from models.diffusion import diffusion_models
 from models.module import vqvae, voice_encoder
 
-def get_model(model_str: str):
-    """모델 클래스 변수 설정
-    Args:
-        model_str (str): 모델 클래스명
-    Note:
-        model_str 이 모델 클래스명으로 정의돼야함
-        `model` 변수에 모델 클래스에 해당하는 인스턴스 할당
-    """
+def get_model(model_config):
+    model_str = model_config['model_name']
+    
     if model_str == 'VQVAE':
-        return vqvae.VQVAE
+        return vqvae.VQVAE(model_config['args'])
     elif model_str == 'LDM':
-        return diffusion_models.LatentDiffusion
+        return diffusion_models.LatentDiffusion(model_config['args'])
     elif model_str == 'SF2F':
-        return voice_encoder.SF2FEncoder
+        return voice_encoder.SF2FEncoder(model_config['args'])
+    elif model_str === 'UNET':
+        return diffusion_models.UNetWithCrossAttention()
     # elif model_str == 'CLIP':
     #     return diffusion_models.CLIP
+
+
+def build_model(model_config):
+    model_list = []
+    for i in range(model_config):
+        model = get_model(model_config[i]['model_name'], model_config[i]['args'])
+        model_list.append(model)
+
+    return model
